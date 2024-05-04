274abe61444520c7bfa7a060529106c5039627c7
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-05-04 17:27:06 +0900

    Update timetable_system_load.php

--- timetable_system_load.php
+++ timetable_system_load.php
@@ -10,23 +10,28 @@ $dbname = "timetable_system"; // 사용할 데이터베이스 이름
 // MySQL 서버에 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$data = json_decode(file_get_contents('php://input'), true);
-$object_code = $data['object_code'];
-
 // 연결 확인
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 // 쿼리 작성
-$sql = "SELECT which, floor, class_number, class_name, device_code, width, height, other, wifi, top_value, left_value FROM classroomDB WHERE object_code = $object_code";
+$sql = "SELECT which, floor, class_number, class_name, device_code, width, height, other, wifi, top_value, left_value, object_code FROM classroomDB";
 
 // 쿼리 실행
 $result = $conn->query($sql);
 
+// 결과를 배열로 저장
+$rows = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $rows[] = (object)$row; // 각 행을 객체로 저장
+    }
+}
+
 // JSON 형식으로 출력
 header('Content-Type: application/json; charset=utf-8'); // UTF-8로 JSON 형식 설정
-echo json_encode($result, JSON_UNESCAPED_UNICODE); // JSON_UNESCAPED_UNICODE 옵션을 사용하여 유니코드 문자를 이스케이프하지 않도록 설정
+echo json_encode($rows, JSON_UNESCAPED_UNICODE); // JSON_UNESCAPED_UNICODE 옵션을 사용하여 유니코드 문자를 이스케이프하지 않도록 설정
 
 // 연결 종료
 $conn->close();
