9040eff9dbd5db0f548de74f2be3c3ec842568e1
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-05-04 18:21:30 +0900

    Update timetable_system_add.php

--- timetable_system_add.php
+++ timetable_system_add.php
@@ -20,6 +20,8 @@ $width = $data['width'];
 $height = $data['height'];
 $other = $data['other'];
 $wifi = $data['wifi'];
+$top = $data['top'];
+$left = $data['left'];
 
 // 데이터베이스 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -30,8 +32,8 @@ if ($conn->connect_error) {
 }
 
 // SQL 쿼리 작성
-$sql = "INSERT INTO classroomDB (object_code, which, floor, class_number, class_name, device_code, width, height, other, wifi) 
-VALUES ('$object_code', '$which_select', '$floor_select', '$class_number', '$class_name', '$device_code', '$width', '$height', '$other', '$wifi')";
+$sql = "INSERT INTO classroomDB (object_code, which, floor, class_number, class_name, device_code, width, height, other, wifi, top_value, left_value) 
+VALUES ('$object_code', '$which_select', '$floor_select', '$class_number', '$class_name', '$device_code', '$width', '$height', '$other', '$wifi', '$top', '$left')";
 
 // 쿼리 실행
 if ($conn->query($sql) === TRUE) {
