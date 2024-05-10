39f0e5e944ab0751da3a95aacdbd10d8cd46053a
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-05-10 19:07:24 +0900

    Update admin_login.js

--- admin_login.js
+++ admin_login.js
@@ -8,8 +8,8 @@ function checkLogin() {
         if (this.readyState == 4 && this.status == 200) {
             // PHP 파일에서 반환한 결과를 확인합니다.
             if (this.responseText === "true") {
-                sendSuccessToTimetableSystem();
-                
+                localStorage.setItem('loginTF', "true");
+                window.location.href = "timetable_system.html";
             } else {
                 alert("아이디 또는 비밀번호가 올바르지 않습니다.");
             }
@@ -22,15 +22,5 @@ function checkLogin() {
 
 
 function sendSuccessToTimetableSystem() {
-    // AJAX를 사용하여 timetable_system.html에 값을 전송합니다.
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // AJAX 요청이 성공적으로 완료된 후에 timetable_system.html로 리디렉션합니다.
-            window.location.href = "timetable_system.html";
-        }
-    };
-    xhttp.open("POST", "timetable_system.html", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("loginSuccess=true");
+    window.location.href = "timetable_system.html";
 }
