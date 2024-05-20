b47580df025f698e1d57d68e743e3a923e8231b0
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-05-21 00:36:41 +0900

    Update timetable_system.js

--- timetable_system.js
+++ timetable_system.js
@@ -726,26 +726,24 @@ function logincheck(){
         mouse_info='up';
     });
   
-    let m_Running = false;
     document.addEventListener('mousemove', function(event){
-      if(m_Running) return;
-      m_Running = true;
 
       if(activeButton===document.querySelectorAll('.menu_button')[2]&&mouse_info==='down'){
         aX=event.clientX;
         aY=event.clientY;
   
-        choice_classrooms.forEach(function(element){
-          //var searchdata = classroomDB(element.id);
-          var Left = String(parseFloat(element.style.left) + (aX-bX))+'px';
-          var Top = String(parseFloat(element.style.top) + (aY-bY))+'px';
-          
-          element.style.left = Left;
-          element.style.top = Top;
-        });
+        for (var i = 0; i < choice_classrooms.length; i++) {
+            var element = choice_classrooms[i];
+            // var searchdata = classroomDB(element.id);
+            var Left = String(parseFloat(element.style.left) + (aX - bX)) + 'px';
+            var Top = String(parseFloat(element.style.top) + (aY - bY)) + 'px';
+            
+            element.style.left = Left;
+            element.style.top = Top;
+        }
+        
         bX=aX;
         bY=aY;
-        m_Running=false;
       }
     });
   
