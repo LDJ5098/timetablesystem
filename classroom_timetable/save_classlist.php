51d71b14b2834e2535fbaf9d32fc8a4f278fb123
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-07-02 16:58:28 +0900

    Add files via upload

---
+++ classroom_timetable/save_classlist.php
@@ -0,0 +1,34 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+// MySQL 계정 정보
+$servername = "localhost";
+$username = "ser";
+$password = "0000";
+$dbname = "timetable_system";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 연결 확인
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// JSON으로 전송된 데이터를 수신
+$data = json_decode(file_get_contents('php://input'), true);
+
+$object_code = $data['object_code'];
+$maindata = json_encode($data['maindata']);
+
+// 준비된 SQL 문
+$sql = "INSERT INTO classlist (object_code, maindata) VALUES ('$object_code', '$maindata')
+        ON DUPLICATE KEY UPDATE maindata='$maindata'";
+
+// 데이터베이스에 저장
+if ($conn->query($sql) === TRUE) {
+    echo "Data successfully inserted or updated.";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
