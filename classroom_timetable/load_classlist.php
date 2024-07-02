a79c9b91b2d9eb19ceb7a5bffc6d9573a44a2bf4
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-07-02 17:06:05 +0900

    Add files via upload

---
+++ classroom_timetable/load_classlist.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+// MySQL 계정 정보
+$servername = "localhost";
+$username = "ser";
+$password = "0000";
+$dbname = "timetable_system";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 연결 확인
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// object_code를 받아옴 (예시로 임의로 지정)
+$object_code = $_GET['object_code'];
+
+// SQL 쿼리를 준비
+$sql = "SELECT maindata FROM classlist WHERE object_code = '$object_code'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // 데이터가 존재하는 경우
+    $row = $result->fetch_assoc();
+    $maindata = $row['maindata'];
+    
+    // JSON 형태로 반환
+    echo json_encode(array('maindata' => json_decode($maindata)));
+} else {
+    // 데이터가 없는 경우
+    echo json_encode(array('maindata' => null));
+}
+
+$conn->close();
+?>
