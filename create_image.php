52aa92cad8b72f2655b6147b7e971fda0216549a
LDJ5098 <151591589+LDJ5098@users.noreply.github.com>
2024-07-08 21:34:57 +0900

    Update create_image.php

--- create_image.php
+++ create_image.php
@@ -197,7 +197,7 @@ $endtime = $result_obj['endtime'] ?? null;
 $startTime_convert = convertMinutesToTime($starttime);
 $endTime_convert = convertMinutesToTime($endtime);
 
-echo "교실명 : " . $classname . "\n" . "교수명 : " . $professor . "\n" . "수업시작 시간 : " . $startTime_convert . "\n" . "수업 끝나는 시간 : " .$endTime_convert . "\n" . "버전 : " . $version . "\n" . "기기코드 : " . $code . "\n" . "교실key(ID) : " . $key . "\n";
+echo "교실명 : " . $classname . "," . "교수명 : " . $professor . "," . "수업시작 시간 : " . $startTime_convert . "," . "수업 끝나는 시간 : " .$endTime_convert . "," . "버전 : " . $version . "," . "기기코드 : " . $code . "," . "교실key(ID) : " . $key . ",";
 drawClassInfo($classname, $professor, $startTime_convert, $endTime_convert, $version, $code, $key);
 
 
